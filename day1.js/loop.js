1c32f953e7f6687dd45aeb0c86cb15aacaea7312
Shivm75 <shivamguptaskg6@gmail.com>
2025-09-17 15:03:25 +0530

    commit

---
+++ day1.js/loop.js
@@ -0,0 +1,13 @@
+let row = 4
+let col = 2; row = 1;
+for(let i=0; i <= row; i++){
+    
+    for(let j=1;j<=col;j++){
+        if(i+j>row && j - i <= row){
+
+        }else{
+
+        }
+    }
+    console.log(line);
+}
---
+++ day1.js/scope.js
@@ -0,0 +1,10 @@
+//console.log(name);
+console.log(fun);
+//console.log(fun());
+function fun(){
+    var name = "Rohini";
+    name = "Mittle"
+    console.log(name);
+    return name;
+}
+console.log(fun(),"from invokation");
